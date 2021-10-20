50c31e0f49cc5e938e9081785ebc6950e55bca19
xTheCoderx <justaguyoney@gmail.com>
2021-10-21 02:24:53 +1300

    Added more information on the compilation stage of how the C++ code gets converted into executable.

--- main.cpp
+++ main.cpp
@@ -5,5 +5,10 @@ int main() {
 
     std::cout << "If you have header files, the preprocessor will replace the 'include' statement with the content of the corresponding file.\n";
 
+    std::cout << "Any macros that the user has defined will be expanded.\n";
+
+    std::cout << "The next step is compilation, which is performed by the compiler.\n";
+    std::cout << "The code is checked for the correct syntax. If there are no errors, the code will be converted into object code. A .obj file will also be generated.\n";
+
     return 0;
 }
