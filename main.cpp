f48de7403abf6f1ab17efdf650246e7bb6557f2d
xTheCoderx <justaguyoney@gmail.com>
2021-10-21 02:22:26 +1300

    Added more information on the first stages of preprocessing.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 int main() {
-    std::cout << "Let's learn how C++ source code gets converted into an executable." << std::endl;
+    std::cout << "Let's learn how C++ source code gets converted into an executable.\n";
+    std::cout << "The first stage is preprocessing, which processes all the statements beginning with the '#' sign.\n";
+
+    std::cout << "If you have header files, the preprocessor will replace the 'include' statement with the content of the corresponding file.\n";
 
     return 0;
 }
